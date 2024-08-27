82b199dc34fe91e179293d8a5961512e5d1684d4
PeliRed <pelired@gmail.com>
2024-08-27 19:06:49 -0400

    add enrutador, archivo _layout.js e Index.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex flex-col justify-around items-center h-screen">
+      <StatusBar style="auto"></StatusBar>
+      <Text>Inicio</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
