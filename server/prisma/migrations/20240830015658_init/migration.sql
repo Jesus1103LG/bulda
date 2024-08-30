ac4132fd7aa387e811b4327bde45d560bf6b5bf2
PeliRed <pelired@gmail.com>
2024-08-29 22:08:23 -0400

    create: prisma/(prisma.moduel, prisma.service), migracion de prueba db

---
+++ server/prisma/migrations/20240830015658_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Prueba` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `age` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
